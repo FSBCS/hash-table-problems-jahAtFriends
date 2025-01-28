e0e76f0e09592cccd37d4ca1c6027a510897600f
Joel Hammer <jhammer@friendsbalt.org>
2025-01-28 14:13:36 +0000

    Add solutions

--- p3.py
+++ p3.py
@@ -1,2 +1,14 @@
 def count_subarrays_with_sum(arr, sum):
-    pass
+    initial_sums = {0:1}
+    result = 0
+    current_sum = 0
+
+    for val in arr:
+        current_sum += val
+
+        if (current_sum - sum) in initial_sums:
+            result += initial_sums[current_sum - sum]
+
+        initial_sums[current_sum] = initial_sums.get(current_sum, 0) + 1
+    
+    return result
